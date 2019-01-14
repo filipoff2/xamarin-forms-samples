0a9b6e9c60764b50e03f745438bd6422da6be0f1
Bohdan Benetskyi (PGS Software) <bbenetskyi@pgs-soft.com>
2019-01-14 12:47:02 +0100

    Add Less into Monkey App

---
+++ UserInterface/MonkeyAppPicker/MonkeyApp/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less");
+
+
+gulp.task("Build Test Less File", function () {
+    return gulp.src('Styles/Test.less')
+        .pipe(less())
+        .pipe(gulp.dest('Styles/css'));
+});
+
+
+
+gulp.task('default', ['Build Test Less File']);
